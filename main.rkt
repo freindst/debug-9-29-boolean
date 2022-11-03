96c3dc169e68322cd766cde1503e404b9aefed25
blade <yiming.liao@cuw.edu>
2022-11-03 10:10:21 -0500

    make it runnable

--- main.rkt
+++ main.rkt
@@ -32,7 +32,7 @@
 
 (define env '((global (a 1) (b 2) (c 5))))
 
-;(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
+(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
 ;(define sample-code '(local-vars ((p c)) (math / a p)))
 (displayln (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
