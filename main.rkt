fa096cbfc5ace7b94fa0467c9419a2c14d07af93
blade <yiming.liao@cuw.edu>
2022-11-08 11:11:17 -0600

    unfinished let expression update

--- main.rkt
+++ main.rkt
@@ -32,8 +32,8 @@
 
 (define env '((global (a 1) (b 2) (c 5))))
 
-(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
-;(define sample-code '(local-vars ((p c)) (math / a p)))
+;(define sample-code '(call (function (r) (local-vars ((p 100)) (math / r p)) ) (a)))
+(define sample-code '(local-vars ((a 7) (b a) (x b)) (math + x a)))
 (displayln (neo-parser sample-code))
 (define parsed-neo-code (neo-parser sample-code))
 (run-neo-parsed-code parsed-neo-code env)
