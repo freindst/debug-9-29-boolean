21a4a1346f3fa7a5b9133ff03ad0eb0b648b3c8b
blade <yiming.liao@cuw.edu>
2022-12-07 13:59:07 -0600

    object parser

--- main.rkt
+++ main.rkt
@@ -6,8 +6,27 @@
 
 (define env '((global (a 1) (b 2) (c 5))))
 
-(define sample-code '(block (assign x 8) (print x))) ;no output
+(define sample-code
+  '(register player
+             (class Person
+               (fields
+                (name
+                 age)
+                )
+               (constructor
+                (name age)
+                ((assign (this name) name)
+                 (assign (this age) age)
+                 )
+                )
+                (methods
+                 (
+                  (call () (print (this name)))
+                  )
+                 )
+                )
+               )
+             )
 (displayln (neo-parser sample-code))
-(define parsed-neo-code (neo-parser sample-code))
-(run-neo-parsed-code parsed-neo-code env)
-
+;(define parsed-neo-code (neo-parser sample-code))
+;(run-neo-parsed-code parsed-neo-code env)
